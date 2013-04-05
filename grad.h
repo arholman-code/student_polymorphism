bd4ed242e715003a55a84ebdac0b65eab03846be
arholman-code <arholman.code@gmail.com>
2013-04-05 11:56:42 -0300

    Create grad.h

---
+++ grad.h
@@ -0,0 +1,21 @@
+/**********************************************************************
+Andrew Holman
+This is my own work.
+***********************************************************************
+
+***********************************************************************
+CSC 330, Project 1
+The following program will implement a grad class that will use the 
+student class as a base for this derived class.
+**********************************************************************/
+
+#ifndef GRAD_H
+#define GRAD_H
+
+#include <iostream>
+using namespace std;
+
+class grad
+{}
+
+#endif
